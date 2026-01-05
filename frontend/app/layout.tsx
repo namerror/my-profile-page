44b64dfeb0ceecf97ecc5aaf70aae9600de35901
Leon Long <44743690+namerror@users.noreply.github.com>
2026-01-05 17:44:16 +0000

    Add: frontend element optimizations - favicons and manifest to metadata in layout.tsx and files - set color scheme to light

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "My Profile Page",
   description: "Interactive portfolio and project dashboard",
+  icons: [
+    { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon_io/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon_io/favicon-16x16.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/favicon_io/apple-touch-icon.png' },
+  ],
+  manifest: '/favicon_io/site.webmanifest'
 };
 
 export default function RootLayout({
