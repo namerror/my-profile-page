58018fc0d2762d149337c882f2a1221b62b5c46a
Leon Long <44743690+namerror@users.noreply.github.com>
2026-01-15 21:00:58 +0000

    Fix Routing Issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .models import *
 # from .db.session import engine
 # from .db.base import Base
-from .api import projects, skills, auth, learnings, contact
+from .api import projects, skills, auth, learnings, contact, categories
 import os
 from dotenv import load_dotenv
 
@@ -29,6 +29,7 @@ app.include_router(skills.router)
 app.include_router(learnings.router)
 app.include_router(auth.router)
 app.include_router(contact.router)
+app.include_router(categories.router)
 
 @app.get("/")
 def read_root():
